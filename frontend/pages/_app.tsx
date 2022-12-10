9045a163cf612a1ab9efb33a0e31b6467396d2e0
Amir.H Ebrahimi <ah.ebrahimi@phinix.com>
2022-12-11 01:45:23 +0330

    feat: add material ui

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {CacheProvider, EmotionCache} from '@emotion/react';
+import type {AppProps} from 'next/app'
+import Head from 'next/head';
+import * as React from "react";
+import createEmotionCache from 'src/config/createEmotionCache';
+import {MuiThemeProvider} from "src/providers/mui-theme-provider";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
+}
+
+
+export default function App(props: MyAppProps) {
+    const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="viewport" content="initial-scale=1, width=device-width"/>
+            </Head>
+
+            <MuiThemeProvider>
+                <Component {...pageProps} />
+            </MuiThemeProvider>
+        </CacheProvider>
+    );
 }
---
+++ frontend/src/providers/mui-theme-provider.tsx
@@ -0,0 +1,12 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import {ThemeProvider} from "@mui/material/styles";
+import * as React from "react";
+import {PropsWithChildren} from "react";
+import theme from "src/config/theme";
+
+export function MuiThemeProvider({children}: PropsWithChildren<unknown>) {
+    return <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        {children}
+    </ThemeProvider>;
+}
